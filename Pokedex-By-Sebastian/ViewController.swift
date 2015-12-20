aef07d3151bd6c700b8a9e126d4ec8ccbcb05dc5
sebastian brukalo <seba368@gmail.com>
2015-12-20 13:21:26 -0600

    Initial Commit

---
+++ Pokedex-By-Sebastian/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-By-Sebastian
+//
+//  Created by Sebastian Brukalo on 12/20/15.
+//  Copyright © 2015 Sebastian Brukalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
